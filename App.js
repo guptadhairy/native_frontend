e7305864956705d65286085f449d7e34ed127144
Dhairya Gupta <dhairyagupta@Dhairyas-MacBook-Air.local>
2024-01-30 18:34:12 +0530

    Demo for macbook

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on Dhiru Apple badhiya bhai maza aagya . React native seekhega tera bhai ab </Text>
       <StatusBar style="auto" />
     </View>
   );
